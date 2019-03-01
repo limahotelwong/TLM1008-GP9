d10ccef8c28749b969bd6fa4e6b414ad853454d7
zfzenryu <47657414+zfzenryu@users.noreply.github.com>
2019-03-01 10:42:01 +0800

    PSEUDO 1

---
+++ TLM1008-GP9/TLM1008-GP9/PSEUDOCODES.cpp
@@ -0,0 +1,8 @@
+/*
+
+
+We can start with basic scheduling with 1 master drone
+Assume every thing is ideal , battery etc then assume it starts a central hub then delivers to point to point , then after back to hub eg hub -> point a -> point b -> (after finish all points )hub
+
+
+*/
